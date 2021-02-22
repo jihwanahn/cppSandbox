8fac251e4e7aef5e90abfea8189c7862332adc03
Jihwan <k1tstw@tutanota.com>
2021-02-22 23:54:41 +0900

    played greedy algo

--- main.cpp
+++ main.cpp
@@ -1,65 +1,119 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include <d3dx9math.h>
-
+#include <algorithm>
+#include <map>
+#include <array>
 
 using namespace std;
 
-typedef long long ll;
-
-ll binpow(ll a, ll b) {
-    ll res{ 1 };
-
-    while (b > 0) {
-        if (b & 1) res *= a;
-
-        a = a * a;
-        cout << "Before : " << b << endl;
-        b >>= 1;
-        cout << "After : " << b << endl;
-    }
-
-    return res;
+template <size_t N>
+void print(std::array<int, N>& arr) {
+	for (auto ele : arr)
+		std::cout << ele << ", ";
 }
 
+class A {
+public:
+	A() { cout << "HI A" << endl; }
+	virtual ~A() { cout << "BYE A" << endl; }
+};
 
-long long binpow(long long a, long long b, long long m) {
-    a %= m;
-    long long res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
+class B : public A {
+public:
+	B() { cout << "HI B" << endl; };
+	virtual ~B() { cout << "BYE B" << endl; }
+};
+
+int changeMoney(int N) {
+	int count = 0;
+
+	array<int, 4> moneyList = { 500, 100, 50, 10 };
+
+	for (auto money : moneyList) {
+		count += N / money;
+		N %= money;
+	}
+
+	return count;
+}
+
+int bigNumRule() {
+	// 동빈북 2번
+	// 배열의 크기 N, 숫자가 더해지는 횟수 M, K를 초과하여 더해질 수 없는 것
+	int N = 0, M = 0, K = 0;
+	cin >> N >> M >> K;
+	cin.ignore();
+
+	vector<int> InArr;
+
+	while (N > 0)
+	{
+		int i;
+		cin >> i;
+		InArr.push_back(i);
+
+		--N;
+	}
+
+	sort(InArr.begin(), InArr.end());
+	int ArrSize = InArr.size();
+	int largest1 = InArr[ArrSize - 1];
+	int largest2 = InArr[ArrSize - 2];
+
+	int times = (M / (K + 1)) * K;
+	times += M % (K + 1);
+
+	int sum = 0;
+
+	sum += times * largest1;
+	sum += largest2 * (M - times);
+
+	return sum;
+}
+
+void NumCardGame() {
+	int n, m, result;
+
+	// N, M을 공백을 기준으로 구분하여 입력 받기
+	cin >> n >> m;
+
+	// 한 줄씩 입력 받아 확인하기
+	for (int i = 0; i < n; i++) {
+		// 현재 줄에서 '가장 작은 수' 찾기
+		int min_value = 10001;
+		for (int j = 0; j < m; j++) {
+			int x;
+			cin >> x;
+			min_value = min(min_value, x);
+		}
+		// '가장 작은 수'들 중에서 가장 큰 수 찾기
+		result = max(result, min_value);
+	}
+
+	cout << result << '\n'; // 최종 답안 출력
+}
+
+void UntilGetOne(int N = 0, int K = 0) {
+	int MinCount{ 0 };
+
+	while (N != 1) {
+		if (N % K == 0) {
+			N /= K;
+		}
+		else {
+			--N;
+		}
+		++MinCount;
+	}
+	cout << MinCount << endl;
 }
 
 int main() {
-    
-    // ll i = 2;
-    // ll j = 8;
-    //
-    // cout << binpow(i, j) << endl;    
-    // int a{ 1 };
-    // int b{ 1 };
-    // int bb{ 2 };
-    // auto c = a & b;
-    // auto d = a | bb;
-    // cout << c << " " << d;
 
-    D3DXVECTOR3 u{ 1.f, 0.f, 1.f };
-    D3DXVECTOR3 v{ 0.f, 1.f, 0.f };
-
-    // u == v ? cout << "Same Vector!!!" << endl : cout << "Diff Vector!!" << endl;
-    
-    // cout << D3DXVec3Length(&u) << endl;
-    // D3DXVec3Normalize(&v, &u);
-
-    // cout << v.x << " " << v.y << " " << v.z << endl;
-    // cout << D3DXVec3Length(&v) << endl;
-
-    D3DXVECTOR3 newU = u * 3;
-
+	// Greed done
+	//asdf;
 
 	return 0;
 }
